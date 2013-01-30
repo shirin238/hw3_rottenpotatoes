af0db5574d3c458544d06eb90ee0c6640da2dc56
shirin238 <cinzia.faenza@gmail.com>
2013-01-30 13:06:24 -0800

    100%

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -36,6 +36,24 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   end
 end
 
-#last added
+Then /^I should see all the movies$/ do
+  rows = page.all("table#movies tbody tr td[1]").map! {|t| t.text}
+  assert ( rows.size == Movie.all.count )
+end
+
+Then /^I should see the following ratings: (.*)/ do |rating_list|
+  ratings = page.all("table#movies tbody tr td[2]").map! {|t| t.text}
+  rating_list.split(",").each do |field|
+    assert ratings.include?(field.strip)
+  end
+end
+
+Then /^I should not see the following ratings: (.*)/ do |rating_list|
+  ratings = page.all("table#movies tbody tr td[2]").map! {|t| t.text}
+  rating_list.split(",").each do |field|
+    assert !ratings.include?(field.strip)
+  end
+end
+
 
 
